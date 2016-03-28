0b83230b35b97848d6aac3cd32f6a443b717c3e7
Eric Merritt <eric@merritt.tech>
2016-03-28 14:14:13 -0700

    beamPackages: Add support for Mix and Erlang.mk

--- pkgs/sunlight/system/thorndyke/default.nix
+++ pkgs/sunlight/system/thorndyke/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, sunlight, erlangPackages, which, nettools }:
+{ stdenv, sunlight, beamPackages, which, nettools }:
 
 
-erlangPackages.buildRebar3 {
+beamPackages.buildRebar3 {
   name = "thorndyke";
   version = "0.0.0+build.10.g2ad02c1";
   src = sunlight.fetch {name = "thorndyke";version = "0.0.0+build.10.g2ad02c1"; sha256 = "6375b979f6880aef833015ef8da3071f1746e21801e7f10167abf883bf34a3f0";};
 
   buildInputs = [ which nettools ];
 
-  erlangDeps = with erlangPackages; [ elli ];
+  erlangDeps = with beamPackages; [ elli ];
 
   installPhase = ''
     runHook preInstall
