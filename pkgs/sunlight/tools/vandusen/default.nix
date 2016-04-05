c9020be370a87b6b3f33b8a0ba46751d6c0d97a4
Guido Zgraggen <guido@sunlightpayments.com>
2016-04-05 09:44:36 -0700

    sunlight.vandusen: update to 0.0.3+build.4.ga3b99d1

--- pkgs/sunlight/tools/vandusen/default.nix
+++ pkgs/sunlight/tools/vandusen/default.nix
@@ -1,17 +1,30 @@
 { sunlight, haskellPackages, stdenv, moreutils }:
 
 with haskellPackages; mkDerivation {
-        pname = "vandusen";
-        version = "0.0.2";
-        src = sunlight.fetch {name = "vandusen";version = "0.0.2"; sha256 = "0qfwc8sg1psqcqcdchz754zh0kyblsr5idcf5i7npl0k4dbdqcar";};
-        isLibrary = false;
-        isExecutable = true;
-        dontStrip = true;
-        buildDepends = [ moreutils hlint ];
-        testHaskellDepends = [ tasty doctest ];
-        executableHaskellDepends = [ base cmdargs shelly split text
-                                     lens semver MissingH HStringTemplate
-                                     system-filepath github formatting ];
-        description = "Automation commands for sunlight systems";
-        license = stdenv.lib.licenses.unfree;
+  pname = "vandusen";
+  version = "0.0.3+build.4.ga3b99d1";
+  src = sunlight.fetch {name = "vandusen";version = "0.0.3"; sha256 = "1jhx5y0d5jnhkcfwhjhldbyhp0sx19grigbkpahlm1m7sf1fd83v";};
+
+  isLibrary = true;
+  isExecutable = true;
+  dontStrip = true;
+
+  buildDepends = [
+    moreutils haskellPackages.cabal-install
+    haskellPackages.hindent
+    haskellPackages.hlint
+  ];
+
+  libraryHaskellDepends = [
+    base formatting github HStringTemplate lens MissingH semver shelly
+    split system-filepath text
+  ];
+
+  executableHaskellDepends = [ base cmdargs mtl shelly text ];
+  testHaskellDepends = [
+    base cmdargs doctest shelly split tasty text
+  ];
+
+  description = "Automation commands for sunlight systems";
+  license = stdenv.lib.licenses.unfree;
 }
