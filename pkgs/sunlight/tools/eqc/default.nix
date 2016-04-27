4de17adb196b82e5cf97978c03ce88ddfb2bfcb0
Jon Meredith <jon@sunlightpayments.com>
2016-04-27 10:17:29 -0700

    Add setup hook to include EQC beams in ERL_LIBS paths.

--- pkgs/sunlight/tools/eqc/default.nix
+++ pkgs/sunlight/tools/eqc/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchurl, unzip }:
+{ stdenv, fetchurl, writeText, unzip }:
 
 stdenv.mkDerivation rec {
   version = "1.37.2";
@@ -9,11 +9,15 @@ stdenv.mkDerivation rec {
     sha256 = "198ar1yipssxi43d01zca7rbj4ra7b4jl1mhpa5i9rsxgb2rkh4l";
   };
 
-  buildInputs = [ unzip ];
+  buildInputs = [ unzip ]; # xdg-open used in doc helper scripts
 
   outputs = [ "out" "docs" "emacs" "examples" ];
   
   builder = ./builder.sh;
+
+  setupHook = writeText "setuphook.sh" ''
+     addToSearchPath ERL_LIBS "$1/lib/erlang/lib/"
+       '';
 
   meta = {
     description = "Property based testing tool by Quviq";
