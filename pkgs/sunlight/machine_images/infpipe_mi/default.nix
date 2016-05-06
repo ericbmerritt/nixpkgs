9b2c9b1b6d8b0fbf66a1ba78d2ae4d413fdfc4bf
Guido Zgraggen <guido@sunlightpayments.com>
2016-05-06 11:29:10 -0700

    infpipe_mi : 0.0.0+build.55.g2ea3f94

---
+++ pkgs/sunlight/machine_images/infpipe_mi/default.nix
@@ -0,0 +1,57 @@
+# USED BY infopipe (sunlight tooling) TO GENERATE `infpipe/default.nix`,
+# ONLY EDIT THE `expression.st` TEMPLATE in the project git
+# repository.
+{ stdenv, sunlight, glibcLocales, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  name = "infpipe_mi";
+  version = "0.0.0+build.55.g2ea3f94";
+
+  src = sunlight.fetch {name = "infpipe_mi";version = "0.0.0+build.55.g2ea3f94"; sha256 = "16yb1y32lax8qcviiyzc2vs8wj3jyvz8sarxr2dc5q2nklhijw7z";};
+
+  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+
+  LANG="en_US.UTF-8";
+  LC_CTYPE=LANG;
+  LC_NUMERIC=LANG;
+  LC_TIME=LANG;
+  LC_COLLATE=LANG;
+  LC_MONETARY=LANG;
+  LC_MESSAGES=LANG;
+  LC_PAPER=LANG;
+  LC_NAME=LANG;
+  LC_ADDRESS=LANG;
+  LC_TELEPHONE=LANG;
+  LC_MEASUREMENT=LANG;
+  LC_IDENTIFICATION=LANG;
+  LC_ALL=LANG;
+
+
+  buildInputs = with sunlight; [ infcli infpipe makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/sunlight/configurations/infpipe_mi
+    mkdir -p $out/sunlight/packer/infpipe_mi
+    mkdir -p $out/bin/
+
+    cp src/nix/* $out/sunlight/configurations/infpipe_mi/
+    cp src/packer/* $out/sunlight/packer/infpipe_mi/
+    cp bin/infpipe_mi $out/bin/infpipe_mi
+
+    chmod a+x $out/bin/infpipe_mi
+
+    wrapProgram $out/bin/infpipe_mi \
+       --suffix PATH : ${sunlight.infcli}/bin \
+       --add-flags "--configuration $out/sunlight/configurations/infpipe_mi/qemu.nix"
+
+    runHook postInstall
+  '';
+
+
+  meta = {
+    description = "Descriptions for the sinlightapi machine image";
+    license = stdenv.lib.licenses.unfree;
+  };
+}
