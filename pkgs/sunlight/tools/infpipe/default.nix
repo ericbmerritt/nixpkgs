f71a752f706cd9110d71ede571b2426c6be79dc0
InfPipe CD System <tech@sunlightpayments.com>
2016-05-10 23:45:24 +0000

    infpipe : 0.0.5+build.8.g5edb620

--- pkgs/sunlight/tools/infpipe/default.nix
+++ pkgs/sunlight/tools/infpipe/default.nix
@@ -1,30 +1,35 @@
 { pkgs, sunlight, haskellPackages, stdenv, moreutils, gitMinimal,
-  makeWrapper, bash, gnumake, gnupg, nix }:
+  bash, gnumake, gnupg, nix, nettools, makeWrapper, openssh}:
 
 with haskellPackages; mkDerivation {
   pname = "infpipe";
-  version = "0.0.5+build.6.ge1d61bc";
-  src = sunlight.fetch {name = "infpipe";version = "0.0.5+build.6.ge1d61bc"; sha256 = "1dsmizj6qj729zy7x11swgxz09745szj9j5bsd635s1wr6g6yx04";};
+  version = "0.0.5+build.8.g5edb620";
+  src = sunlight.fetch {name = "infpipe";version = "0.0.5+build.8.g5edb620"; sha256 = "18yv54ikvqazp4rcvvwf3arqlvbra8lakmcvdwyxk991ybkpqdgl";};
   isLibrary = false;
   isExecutable = true;
   dontStrip = true;
 
-  buildDepends = [ moreutils hlint
-                   sunlight.infcli makeWrapper ];
+  buildDepends = [ moreutils hlint makeWrapper
+                   sunlight.infcli sunlight.public-keys ];
+
   testHaskellDepends = [ tasty doctest ];
+
   executableHaskellDepends = [ base cmdargs shelly aeson github
                                split wreq hslogger time lens bytestring
-                               sunlight.public-keys pkgs.git MissingH
-                               sunlight.infcli gitMinimal
-                               slack-notify-haskell ];
+                               MissingH  slack-notify-haskell ];
 
-   postInstall = ''
+  postInstall = ''
      wrapProgram $out/bin/infpipe \
+         --suffix PATH : ${openssh}/bin \
          --suffix PATH : ${gitMinimal}/bin \
-         --suffix PATH : ${bash}/bin:${gnumake}/bin \
+         --suffix PATH : ${nettools}/bin \
+         --suffix PATH : ${bash}/bin \
+         --suffix PATH : ${gnumake}/bin \
          --suffix PATH : ${sunlight.public-keys}/bin \
-         --suffix PATH : ${gnupg}/bin:${nix}/bin
-   '';
+         --suffix PATH : ${sunlight.infcli}/bin \
+         --suffix PATH : ${gnupg}/bin \
+         --suffix PATH : ${nix}/bin
+  '';
 
   description = "CI System for sunlight";
   license = stdenv.lib.licenses.unfree;
