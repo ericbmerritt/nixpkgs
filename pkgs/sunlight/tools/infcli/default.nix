2975cf81a3108d3025e5c558ff5ef4633739584a
InfPipe CD System <tech@sunlightpayments.com>
2016-05-11 23:25:10 +0000

    infcli : 0.0.6+build.10.gab8b7f6

--- pkgs/sunlight/tools/infcli/default.nix
+++ pkgs/sunlight/tools/infcli/default.nix
@@ -1,38 +1,81 @@
-{ awscli, bash, easyrsa, gitMinimal, gnupg, haskellPackages, makeWrapper,
-  moreutils, nix, sunlight, stdenv }:
+{ awscli
+, bash
+, easyrsa
+, gitMinimal
+, gnupg
+, haskellPackages
+, makeWrapper
+, moreutils
+, nix
+, nix-prefetch-git
+, openssh
+, stdenv
+, sunlight}:
 
 with haskellPackages; mkDerivation {
   pname = "infcli";
-  version = "0.0.6+build.8.g2ea9eb3";
-  src = sunlight.fetch {name = "infcli";version = "0.0.6+build.8.g2ea9eb3"; sha256 = "15wj2pknhwfz0101pnvb6cs53lx9bmbjwyffa9h575mz6mnfp69f";};
+  version = "0.0.6+build.10.gab8b7f6";
+  src = sunlight.fetch {name = "infcli";version = "0.0.6+build.10.gab8b7f6"; sha256 = "008i8f0bz2lgifbjj72m2l29scvkd4jv6w7m75irk9lgdfdvg10i";};
 
   isLibrary = true;
   isExecutable = true;
   dontStrip = true;
 
   buildDepends = [
-    moreutils haskellPackages.cabal-install
+    haskellPackages.cabal-install
     haskellPackages.hindent
     haskellPackages.hlint
+    moreutils
   ];
 
   libraryHaskellDepends = [
-    base formatting github HStringTemplate lens MissingH semver shelly
-    split system-filepath text regex-compat
+    HStringTemplate
+    MissingH
+    base
+    formatting
+    github
+    lens
+    regex-compat
+    semver
+    shelly
+    split
+    system-filepath
+    text
   ];
 
-  executableHaskellDepends = [ base cmdargs mtl shelly text gitMinimal
-                               sunlight.public-keys makeWrapper ];
+  executableHaskellDepends = [
+    base
+    cmdargs
+    gitMinimal
+    makeWrapper
+    mtl
+    nix-prefetch-git
+    openssh
+    shelly
+    sunlight.public-keys
+    text
+  ];
+
   testHaskellDepends = [
-    base cmdargs doctest shelly split tasty text awscli easyrsa
+    awscli
+    base
+    cmdargs
+    doctest
+    easyrsa
+    shelly
+    split
+    tasty
+    text
   ];
 
   postInstall = ''
      wrapProgram $out/bin/infcli \
+         --suffix PAHT : ${nix}/bin \
          --suffix PATH : ${bash}/bin \
          --suffix PATH : ${gitMinimal}/bin \
          --suffix PATH : ${gnupg}/bin \
-         --suffix PAHT : ${nix}/bin \
+         --suffix PATH : ${nix-prefetch-git}/bin \
+         --suffix PATH : ${openssh}/bin \
          --suffix PATH : ${sunlight.public-keys}/bin
    '';
 
