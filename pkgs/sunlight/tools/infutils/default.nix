2ed08f7b1bda35a55759224cf39ffada0bb76e98
InfPipe CD System <tech@sunlightpayments.com>
2016-05-16 22:02:19 +0000

    infutils : 0.0.0+build.20.ga634c4e

--- pkgs/sunlight/tools/infutils/default.nix
+++ pkgs/sunlight/tools/infutils/default.nix
@@ -1,8 +1,8 @@
 { stdenv, sunlight }:
 stdenv.mkDerivation rec {
   name = "infutils";
-  version = "0.0.0+build.17.g0466ebb";
-  src = sunlight.fetch {name = "infutils";version = "0.0.0+build.17.g0466ebb"; sha256 = "1h5qx4dw3qsh0nb9a80vffr9fpy7iw9bq7brzrrfq972qaq8w7dr";};
+  version = "0.0.0+build.20.ga634c4e";
+  src = sunlight.fetch {name = "infutils";version = "0.0.0+build.20.ga634c4e"; sha256 = "1rcv8y714iz4mwhy9rkgnj326x9jwwmzj8vnwy4b4x533lbi8is1";};
   isLibrary = false;
   isExecutable = false;
   dontStrip = true;
