56b800b762c3eb411ce9ca81bffa4a5a1a3dc72d
Jon Meredith <jon@sunlightpayments.com>
2016-06-07 09:06:57 -0700

    Change over to EQC site license so that nixbld builders work.

--- pkgs/sunlight/eqc/default.nix
+++ pkgs/sunlight/eqc/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, writeText, unzip }:
+{ stdenv, fetchurl, writeText, unzip, erlang }:
 
 stdenv.mkDerivation rec {
   version = "1.37.2";
@@ -9,15 +9,28 @@ stdenv.mkDerivation rec {
     sha256 = "198ar1yipssxi43d01zca7rbj4ra7b4jl1mhpa5i9rsxgb2rkh4l";
   };
 
-  buildInputs = [ unzip ]; # xdg-open used in doc helper scripts
+  buildInputs = [ unzip erlang ]; # xdg-open used in doc helper scripts
 
+  dontstrip = true;
   outputs = [ "out" "docs" "emacs" "examples" ];
   
   builder = ./builder.sh;
 
+  register-eqc = writeText "register-eqc.escript" ''
+#! escript -I $out/lib/erlang/eqc-${version}
+main([]) ->
+  eqc:registration("SITEg2wAAAABboAAyLVD6YVWuqa83tGsaTFdev4SghINUzb2RfuZESaxRz0OoMuqIHmKD6adti4ln2WKsHdS4emwtlJC0z026JKEUWxcZLzM6PyWx9xzU3f332TIHj9PFbuDIYhWFX3gki+miMr7ZfqM7CL3X2tPQlM7BpSRgsDJ5cC6kAvvNlZgWWVq"),
+  eqc:start(),
+  init:stop().
+  '';
+
   setupHook = writeText "setuphook.sh" ''
+     echo "### Awaiting feature request for EQC to set location of .quviq2, temporarily overriding HOME ###"
+     export HOME=/tmp
      addToSearchPath ERL_LIBS "$1/lib/erlang/lib/"
-       '';
+     echo yes | ${erlang}/bin/escript ${register-eqc}
+     echo "Set up EQC for $(id -un) with license in $HOME"
+     '';
 
   meta = {
     description = "Property based testing tool by Quviq";
