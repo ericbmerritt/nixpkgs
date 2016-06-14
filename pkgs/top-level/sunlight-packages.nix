a82a01262e0de50bafd1577f479bca7d405472ed
Scott R. Parish <srp@sunlightpayments.com>
2016-06-13 20:23:37 -0700

    add sunlight.packageNames
    This is needed by an upcoming change to infpipe

--- pkgs/top-level/sunlight-packages.nix
+++ pkgs/top-level/sunlight-packages.nix
@@ -33,4 +33,14 @@ let
     sunlightapi_mi = callPackage ../sunlight/sunlightapi_mi {};
     workspace = callPackage ../sunlight/workspace {};
 /* -- END-SUNLIGHT-PACKAGES -- */
+
+    # A list of strings of the packages names owned by this namespace
+    # (excluding 'third-party'). This is used by 'infpipe' as the
+    # list of packages to be built.
+    packageNames =
+      with builtins;
+      let keepName = name: name != "packageNames" &&
+                           name != "third-party" &&
+                           isAttrs (getAttr name self);
+      in filter keepName (attrNames self);
 }; in self
