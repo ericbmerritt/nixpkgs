88f993a0a86f2e9b3f0b380b2ba653048e35115a
InfPipe CD System <tech@sunlightpayments.com>
2016-06-14 21:26:35 +0000

    workspace : 0.0.0+build.19.g5defc94

--- pkgs/sunlight/workspace/default.nix
+++ pkgs/sunlight/workspace/default.nix
@@ -8,8 +8,8 @@
 
 with haskellPackages; mkDerivation {
   pname = "workspace";
-  version = "0.0.0+build.17.g7685de6";
-  src = sunlight.fetch {name = "workspace";version = "0.0.0+build.17.g7685de6"; sha256 = "1d096d7gwv4wm0bmynm6lzfj14w0270xay0lfm71kiyfaqx11yd1";};
+  version = "0.0.0+build.19.g5defc94";
+  src = sunlight.fetch {name = "workspace";version = "0.0.0+build.19.g5defc94"; sha256 = "1d4y2h5cx9cg5wzg3lmqr0ha6y2pmc2nsh07j7rpx5qc8x9is7sf";};
 
   isLibrary = true;
   isExecutable = true;
